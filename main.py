e9b9f939967a84aa12fae3793e78c46331a91ea2
Ivan Trofimov <ivantrofimov052@gmail.com>
2020-11-07 16:21:11 +0300

    # commit 4

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def derterminant(matrix):
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
 
+    if len(matrix) == 1:
+        return matrix[0][0]
+
     all_matrix = []
 
     for i in range(len(matrix)):
@@ -29,10 +32,27 @@ def derterminant(matrix):
 
 
 def inverse(Matrix):
-    pass
+    new_matrix=[]
+
+    for i in range(len(Matrix)):
+        new_matrix.append([])
+        for j in range(len(Matrix)):
+            matrix_detriminant = []
+            for k in range(len(Matrix)):
+                if k != i:
+                    matrix_detriminant.append([])
+                    for l in range(len(Matrix)):
+                        if j != l:
+                            matrix_detriminant[len(matrix_detriminant) - 1].append(Matrix[k][l])
+
+            new_matrix[i].append((derterminant(matrix_detriminant) * ((-1) ** (i+j))) / derterminant(Matrix))
+
+    return new_matrix
 
 
 matrix_1 = [[1, 2], [1, 2]]
 matrix = [[1, 2, 3, 4], [0, 4, 5, 6], [1, 0, 6, 9], [2, 1, 3, 4]]
 print(derterminant(matrix))
 print(np.linalg.det(matrix))
+print(inverse(matrix))
+print(np.linalg.inv(matrix))
