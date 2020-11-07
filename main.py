b3afb1b1fed92012f8aec1a7cbbc82bd60e168a0
Ivan Trofimov <ivantrofimov052@gmail.com>
2020-11-07 17:37:50 +0300

    # commit 5

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def derterminant(matrix):
@@ -50,9 +51,67 @@ def inverse(Matrix):
     return new_matrix
 
 
+def matrix_mult(matrix_1, matrix_2):
+    new_matrix = []
+
+    for i in range(len(matrix_1)):
+        new_matrix.append([])
+        for j in range(len(matrix_2[0])):
+            skalar = 0
+
+            for k in range(len(matrix_1[0])):
+                skalar += matrix_1[i][k] * matrix_2[k][j]
+
+            new_matrix[i].append(skalar)
+
+    return new_matrix
+
+
+def make_rotation_matrix(n, angel):
+    rotation_matrix = []
+
+    for i in range(n):
+        rotation_matrix.append([])
+        for j in range(n):
+            if i == j:
+                rotation_matrix[i].append(1)
+            else:
+                rotation_matrix[i].append(0)
+
+    rotation_matrix[0][0] = math.cos(angel)
+    rotation_matrix[1][1] = math.cos(angel)
+    rotation_matrix[0][1] = math.sin(angel)
+    rotation_matrix[1][0] = - math.sin(angel)
+
+
+    return rotation_matrix
+
+
+def rotation_vector(matrix, angel):
+    rotation_matrix = make_rotation_matrix(len(matrix), angel)
+    new_matrix = []
+
+    for i in range(len(matrix)):
+        new_matrix.append([])
+        skale = 0
+        for j in range(len(matrix)):
+            skale += matrix[j] + rotation_matrix[i][j]
+
+        new_matrix[i].append(skale)
+
+    return new_matrix
+
+
+
 matrix_1 = [[1, 2], [1, 2]]
 matrix = [[1, 2, 3, 4], [0, 4, 5, 6], [1, 0, 6, 9], [2, 1, 3, 4]]
 print(derterminant(matrix))
 print(np.linalg.det(matrix))
 print(inverse(matrix))
 print(np.linalg.inv(matrix))
+a = [[1, 2, 4],  [2, 0, 3]]
+b = [[2, 5], [1, 3], [1, 1]]
+print(matrix_mult(a, b))
+print(make_rotation_matrix(4, 100))
+print('/////////')
+print(rotation_vector([1, 1], 30))
